e189912fbd2309297c40646344fe1721ff5c7e8e
Joaquim Albuquerque <joa_kym_junior@hotmail.com>
2018-09-01 22:03:14 -0400

    Organizando e carregando array com os aliens, utilizando get_node() para chamar um nó

--- scripts/enemie.gd
+++ scripts/enemie.gd
@@ -3,8 +3,29 @@ extends Area2D
 # class member variables go here, for example:
 # var a = 2
 # var b = "textvar"
+export(int, "A" , "B", "C") var tipo = 0
+
+var score = 0
+#carregando o array com os aliens
+#inserindo a pontuação para cada alien
+var atributos = [
+	{
+	texture = preload("res://sprites/InvaderA_sheet.png"),
+	score = 10
+	},
+	{
+	texture = preload("res://sprites/InvaderB_sheet.png"),
+	score = 20
+	},
+	{
+	texture = preload("res://sprites/InvaderC_sheet.png"),
+	score=30
+	}
+]
 
 func _ready():
-	# Called every time the node is added to the scene.
-	# Initialization here
+	var atributo = atributos[tipo]
+	#pedindo um nó
+	get_node("sprite").set_texture(atributo.texture)
+	score = atributo.score
 	pass
